9227181d42567d1e4e5d2df9d2078f0b757350d7
bauassr <bauassr@gmail.com>
2018-03-13 12:42:02 +0530

    Add files via upload

---
+++ Assignment_2.2.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 13 12:22:04 2018
+
+@author: singh.shivam
+"""
+
+for i in range(5):
+     for j in range(i+1):
+         print("*",end="")
+         
+     print("\r")    
+
+for k in range(4):
+    n= 4-k
+    for l in range(n):
+        print("*",end="")
+     
+    print("\r")    
